66c8108dcccd8312f40139c8a2c939a6bc45a168
Mark <markhappy200@gmail.com>
2024-05-20 16:18:13 +0300

    Live version updated

--- routers/customer.py
+++
@@ -1,9 +0,0 @@
-from . import *
-
-router = APIRouter()
-
-"""
-Create the customer router.
-It should include CRUD methods,
-as well as a method to leave an inquiry regarding a vehicle.
-"""
--- routers/vehicle.py
+++
@@ -1,8 +0,0 @@
-from . import *
-
-router = APIRouter()
-
-"""
-Ask the code assistant
-to implement this router for you.
-"""
